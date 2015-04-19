b70cf2a78584fbc9f744e7c19f7ce1195fe352f9
Vinod Rathod <vinod.k.rathod93@gmail.com>
2015-04-19 12:15:33 +0530

    Initial Commit

---
+++ TimeTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeTable
+//
+//  Created by Vinod Rathod on 19/04/15.
+//  Copyright (c) 2015 Vinod Rathod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TimeTable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TimeTable
+//
+//  Created by Vinod Rathod on 19/04/15.
+//  Copyright (c) 2015 Vinod Rathod. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
